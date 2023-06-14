744d7104eb8d088432b38c49050cc94410386995
hikmdgj <hikmdgj@naver.com>
2023-06-14 17:03:23 +0900

    # WARNING: head commit changed in the meantime
    hihi test

---
+++ pdk/src/main/java/com/multicamp/demo/SpirngNcpAi1Application.java
@@ -0,0 +1,13 @@
+package com.multicamp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngNcpAi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngNcpAi1Application.class, args);
+	}
+
+}
---
+++ pdk/src/test/java/com/multicamp/demo/SpirngNcpAi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multicamp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngNcpAi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
